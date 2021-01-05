25addb413fa0874ecc6c794c6fc96b40a2ffbe49
Joakker <joaquinandresleon108@gmail.com>
2021-01-05 17:46:10 -0300

    Testeo de matrices/ con perf

--- matrices/testmatrix.sh
+++ matrices/testmatrix.sh
@@ -1,7 +1,9 @@
 #!/bin/env zsh
 
+PERFS="cpu-cycles,instructions,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,LLC-load-misses,LLC-store-misses,dTLB-load-misses,dTLB-store-misses"
+
 for ((i=1; i < 12; i++)); do
     integer n=$(( 1 << i ))
-    ./nexec $n
+    perf stat -e $PERFS ./prog $n
 done
 
